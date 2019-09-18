b396886e99415e0dde3781ad5abaf8861dde7202
Alex Samuel <alex@alexsamuel.net>
2019-09-18 08:29:02 -0400

    Clean up; use MaybeUninit.

--- src/sys.rs
+++ src/sys.rs
@@ -1,7 +1,8 @@
 extern crate libc;
 
-use libc::{c_int, pid_t, rusage, timeval};
+use libc::{c_int, pid_t, rusage};
 use std::io;
+use std::mem::MaybeUninit;
 use std::string::String;
 use std::vec::Vec;
 
@@ -23,10 +24,9 @@ pub fn fork() -> io::Result<pid_t> {
     let child_pid = unsafe { libc::fork() };
     assert!(child_pid >= -1);
     match child_pid {
-        _ if child_pid < -1 => panic!("fork returned {}", child_pid),
         -1 => Err(io::Error::last_os_error()),
-        0 => Ok(0),
-        _ => Ok(child_pid),
+        _ if child_pid >= 0 => Ok(child_pid),
+        _ => panic!("fork returned {}", child_pid),
     }
 }
 
@@ -36,29 +36,12 @@ pub fn getpid() -> pid_t {
 
 pub fn wait4(pid: pid_t, options: c_int) -> io::Result<(pid_t, c_int, rusage)> {
     let mut status: c_int = 0;
-    let mut usage = rusage {
-        ru_utime: timeval { tv_sec: 0, tv_usec: 0 },
-        ru_stime: timeval { tv_sec: 0, tv_usec: 0 },
-        ru_maxrss: 0,
-        ru_ixrss: 0,
-        ru_idrss: 0,
-        ru_isrss: 0,
-        ru_minflt: 0,
-        ru_majflt: 0,
-        ru_nswap: 0,
-        ru_inblock: 0,
-        ru_oublock: 0,
-        ru_msgsnd: 0,
-        ru_msgrcv: 0,
-        ru_nsignals: 0,
-        ru_nvcsw: 0,
-        ru_nivcsw: 0,
-    };
-    match unsafe {
-        libc::wait4(pid, &mut status, options, &mut usage)
-    } {
-        -1 => Err(io::Error::last_os_error()),
-        child_pid => Ok((child_pid, status, usage)),
+    unsafe {
+        let mut usage = MaybeUninit::<rusage>::uninit();
+        match libc::wait4(pid, &mut status, options, usage.as_mut_ptr()) {
+            -1 => Err(io::Error::last_os_error()),
+            child_pid => Ok((child_pid, status, usage.assume_init())),
+        }
     }
 }
 
