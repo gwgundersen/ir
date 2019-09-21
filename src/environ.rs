faecf23afcb95eeea6836520f83ac9c1004ef35a
Alex Samuel <alex@alexsamuel.net>
2019-09-21 09:31:19 -0400

    Desirialize JSON env inherit.

--- src/environ.rs
+++ src/environ.rs
@@ -1,26 +1,107 @@
-use serde::{Serialize, Deserialize};
+use serde::{Serialize, Deserialize, Deserializer};
+use serde::de::SeqAccess;
 use std::collections::HashMap;
+use std::fmt;
 use std::string::String;
 use std::vec::Vec;
 
-#[derive(Serialize, Deserialize, Debug)]
+//------------------------------------------------------------------------------
+
+#[derive(Serialize, Debug, PartialEq)]
 #[serde(deny_unknown_fields)]
 pub enum EnvInheritSpec {
     None,
     All,
-    Some {
-        vars: Vec<String>,
-    },
+    Vars(Vec<String>),
 }
 
 impl Default for EnvInheritSpec {
-    fn default() -> Self { EnvInheritSpec::None }
+    fn default() -> Self { Self::None }
 }
 
-#[derive(Serialize, Deserialize, Default, Debug)]
+impl<'de> Deserialize<'de> for EnvInheritSpec {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>
+    {
+        struct Visitor;
+        impl<'de> serde::de::Visitor<'de> for Visitor {
+            type Value = EnvInheritSpec;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("true, false, or array of env var names")
+            }
+
+            fn visit_bool<E>(self, v: bool) -> Result<Self::Value, E> {
+                Ok(if v { Self::Value::All } else { Self::Value::None })
+            }
+
+            fn visit_seq<S>(self, mut seq: S) -> Result<Self::Value, S::Error> 
+            where
+                S: SeqAccess<'de>
+            {
+                let mut vars = Vec::new();
+                while let Some(var) = seq.next_element()? {
+                    vars.push(var);
+                }                    
+                Ok(Self::Value::Vars(vars))
+            }
+        }
+
+        deserializer.deserialize_any(Visitor)
+    }
+}
+
+//------------------------------------------------------------------------------
+
+#[derive(Serialize, Deserialize, Default, Debug, PartialEq)]
 #[serde(deny_unknown_fields, default)]
 pub struct EnvSpec {
     pub inherit: EnvInheritSpec,
     pub vars: HashMap<String, String>,
+}
+
+//------------------------------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn assert_json(json: &'static str, expected: EnvSpec) {
+        assert_eq!(
+            serde_json::from_str::<'static, EnvSpec>(json).unwrap(),
+            expected);
+    }
+
+    #[test]
+    fn empty() {
+        assert_json(
+            r#" {} "#, 
+            EnvSpec { inherit: EnvInheritSpec::None, ..Default::default() }
+        );
+    }
+
+    #[test]
+    fn inherit_all() {
+        assert_json(
+            r#" {"inherit": true} "#,
+            EnvSpec { inherit: EnvInheritSpec::All, ..Default::default() }
+        );
+    }
+
+    #[test]
+    fn inherit_vars() {
+        assert_json(
+            r#" {"inherit": ["HOME", "USER", "PATH"]} "#,
+            EnvSpec {
+                inherit: EnvInheritSpec::Vars(vec!(
+                    "HOME".to_string(),
+                    "USER".to_string(),
+                    "PATH".to_string())
+                ),
+                ..Default::default()
+            }
+        );
+    }
 }
 
