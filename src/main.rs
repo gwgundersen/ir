8b7cc807e5b27ea780e048d05d64d88495321373
Alex Samuel <alex@alexsamuel.net>
2019-09-26 08:40:10 -0400

    Logging to stderr.

--- src/main.rs
+++ src/main.rs
@@ -16,29 +16,29 @@ fn main() {
     };
 
     let spec = spec::load_spec_file(&json_path).unwrap_or_else(|err| {
-        println!("failed to load {}: {}", json_path, err);
+        eprintln!("failed to load {}: {}", json_path, err);
         std::process::exit(exitcode::OSFILE);
     });
-    println!("spec: {:?}", spec);
-    println!("");
+    eprintln!("spec: {:?}", spec);
+    eprintln!("");
 
     let env = environ::build(std::env::vars(), &spec.env);
 
     let child_pid = sys::fork().unwrap_or_else(|err| {
-        println!("failed to fork: {}", err);
+        eprintln!("failed to fork: {}", err);
         std::process::exit(exitcode::OSERR);
     });
     if child_pid == 0 {
         let exe = &spec.argv[0];
         let err = sys::execve(exe.clone(), spec.argv.clone(), env).unwrap_err();
-        println!("failed to exec: {}", err);
+        eprintln!("failed to exec: {}", err);
     }
     else {
         let (wait_pid, status, rusage) = sys::wait4(child_pid, 0).ok().unwrap();
         assert_eq!(wait_pid, child_pid);  // FIXME: Errors.
         let result = result::Result { pid: child_pid, status, rusage };
 
-        println!("");
+        eprintln!("");
         result::print(&result);
     }
 
