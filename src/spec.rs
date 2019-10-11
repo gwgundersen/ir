7a439c81b9fa150bdb383b67b1efe2114a73f684
Alex Samuel <alex@alexsamuel.net>
2019-10-11 17:13:06 -0400

    Rename SpecError to Error.

--- src/spec.rs
+++ src/spec.rs
@@ -9,42 +9,42 @@ use std::string::String;
 //------------------------------------------------------------------------------
 
 #[derive(Debug)]
-pub enum SpecError {
+pub enum Error {
     Io(std::io::Error),
     Json(serde_json::error::Error),
 }
 
-impl std::fmt::Display for SpecError {
+impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match *self {
-            SpecError::Io(ref err) => err.fmt(f),
-            SpecError::Json(ref err) => err.fmt(f),
+            Error::Io(ref err) => err.fmt(f),
+            Error::Json(ref err) => err.fmt(f),
         }
     }
 }
 
-impl std::error::Error for SpecError {
+impl std::error::Error for Error {
     fn description(&self) -> &str {
         match *self {
-            SpecError::Io(ref err) => err.description(),
-            SpecError::Json(ref err) => err.description(),
+            Error::Io(ref err) => err.description(),
+            Error::Json(ref err) => err.description(),
         }
     }
 }
 
-impl From<std::io::Error> for SpecError {
-    fn from(err: std::io::Error) -> SpecError {
-        SpecError::Io(err)
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Error {
+        Error::Io(err)
     }
 }
 
-impl From<serde_json::error::Error> for SpecError {
-    fn from(err: serde_json::error::Error) -> SpecError {
-        SpecError::Json(err)
+impl From<serde_json::error::Error> for Error {
+    fn from(err: serde_json::error::Error) -> Error {
+        Error::Json(err)
     }
 }
 
-type Result<T> = std::result::Result<T, SpecError>;
+type Result<T> = std::result::Result<T, Error>;
 
 //------------------------------------------------------------------------------
 
