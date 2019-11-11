949dcdf260c554faf9de9d49f081a2cc9a0c89f8
Alex Samuel <alex@alexsamuel.net>
2019-11-11 01:56:04 -0500

    Extend capture test to memory, base64.

--- tests/integration/test_capture.py
+++ tests/integration/test_capture.py
@@ -1,17 +1,33 @@
+import base64
 import ir
 import pytest
 
 
-@pytest.mark.parametrize("mode", ["tempfile"])
-def test_capture_echo(mode):
+@pytest.mark.parametrize("mode", ["tempfile", "memory"])
+@pytest.mark.parametrize("format", ["text", "base64"])
+def test_capture_echo(mode, format):
     res = ir.run({
         "argv": ["/bin/echo", "Hello, world.", "How are you?"],
         "fds": [
-            ["stdout", {"capture": {"mode": mode}}],
+            [
+                "stdout", {
+                    "capture": {
+                        "mode": mode,
+                        "format": format,
+                    }
+                }
+            ],
         ]
     })
 
     assert res["status"] == 0
-    assert res["fds"]["stdout"]["text"] == "Hello, world. How are you?\n"
+
+    stdout = res["fds"]["stdout"]
+    text = "Hello, world. How are you?\n"
+    if mode == "text":
+        assert stdout["text"] == text
+    elif mode == "base64":
+        assert stdout["encoding"] == "base64"
+        assert stdout["text"] == base64.b64encode(text.encode())
 
 
