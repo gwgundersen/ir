da146540eafdcff4e45cf7267be77198ca7e70c1
Alex Samuel <alex@alexsamuel.net>
2020-02-17 11:11:00 -0500

    Tests for problematic fd setup.

--- tests/integration/test_err.py
+++ tests/integration/test_err.py
@@ -8,9 +8,24 @@ def test_bad_exe():
     """
     Tests error reporting on bad executable.
     """
-    try:
+    with pytest.raises(ir.Errors) as exc_info:
         ir.run1({"argv": ["/usr/bin/bogus"],})
-    except ir.Errors as exc:
-        assert any( "No such file or directory" in e for e in exc.errors )
+    assert any( "No such file or directory" in e for e in exc_info.value.errors )
+
+
+def test_bad_capture_path():
+    """
+    Tests error reporting for a bad capture file path.
+    """
+    with pytest.raises(ir.Errors) as exc_info:
+        ir.run1({
+            "argv": ["/usr/bin/echo", "Hello, world!"],
+            "fds": [
+                ["stdout", {"file": {"path": "/not/a/valid/path",}}],
+                ["stderr", {"file": {"path": "/not/a/valid/path/either",}}],
+            ]
+        })
+    assert any( "failed to set up fd 1" in e for e in exc_info.value.errors )
+    assert any( "failed to set up fd 2" in e for e in exc_info.value.errors )
 
 
